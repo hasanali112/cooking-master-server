8c515fdc49bc7e7e36fb3c0d653c138f55f98c41
hasanali112 <mdhasan.ru.ro@gmail.com>
2023-05-04 11:01:55 +0600

    add cors for get data for client side

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
+const cookDataLoad = require('./data/cookdata.json')
 
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello cook master')
   })
 
-
+app.get('/cook', (req, res)=>{
+    res.send(cookDataLoad)
+})
 
 
 app.listen(port, () => {
